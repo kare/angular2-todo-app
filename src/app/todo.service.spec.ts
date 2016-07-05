cad2c48b7ac2a573463294dfe578e330b122f789
Kare Nuorteva <kare.nuorteva@me.com>
2016-07-05 20:17:29 +0300

    Implement getAllTodos() and addTodo()

--- src/app/todo.service.spec.ts
+++ src/app/todo.service.spec.ts
@@ -6,13 +6,22 @@ import {
   expect, it, xit,
   async, inject
 } from '@angular/core/testing';
+import { Todo } from './todo';
 import { TodoService } from './todo.service';
 
-describe('Todo Service', () => {
+describe('#getAllTodos()', () => {
   beforeEachProviders(() => [TodoService]);
 
-  it('should ...',
+  it('should return an empty array by default',
       inject([TodoService], (service: TodoService) => {
-    expect(service).toBeTruthy();
+    expect(service.getAllTodos()).toEqual([]);
+  }));
+  it('should return all todos',
+      inject([TodoService], (service: TodoService) => {
+    let todo1 = new Todo({title: 'Hello 1', completed: false});
+    let todo2 = new Todo({title: 'Hello 2', completed: true});
+    service.addTodo(todo1);
+    service.addTodo(todo2);
+    expect(service.getAllTodos()).toEqual([todo1, todo2]);
   }));
 });
--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Todo } from './todo';
 
 @Injectable()
 export class TodoService {
+  lastId: number = 0;
+  todos: Todo[] = [];
 
   constructor() {}
 
+  addTodo(todo: Todo): TodoService {
+    if (!todo.id) {
+      todo.id = ++this.lastId;
+    }
+    this.todos.push(todo);
+    return this;
+  }
+
+  getAllTodos(): Todo[] {
+    return this.todos;
+  }
 }
